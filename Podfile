25ee0beb60844da22dcde76cc3cb31a7fa1347db
Danil Shchegol <danilshchegol@icloud.com>
2020-02-23 23:43:19 +0200

    adding pods to source control

---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '11.0'
+
+target 'BirdTest' do
+use_modular_headers!
+pod 'RealmSwift'
+
+end
---
+++ Pods/Target Support Files/Pods-BirdTest/Pods-BirdTest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BirdTest : NSObject
+@end
+@implementation PodsDummy_Pods_BirdTest
+@end
